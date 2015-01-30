7c7add205d1bde8d9c2a390797bb5f720e5cbbe2
james7132 <james.siqi.liu@gmail.com>
2015-01-30 14:56:41 -0500

    XboxController Support

--- Assets/Scripts/PlayerControllers/ControlledAgent.cs
+++ Assets/Scripts/PlayerControllers/ControlledAgent.cs
@@ -2,16 +2,14 @@ using UnityEngine;
 using BaseLib;
 using System.Collections;
 
-public class ControlledAgent : PlayerAgent 
-{
+public class ControlledAgent : PlayerAgent  {
 	private string horizontalMoveAxis;
 	private string verticalMoveAxis;
 	private string fireButton;
 	private string focusButton;
 	private string chargeButton;
 
-	public override void Initialize (PlayerFieldController fieldController, Avatar playerAvatar, PlayerFieldController targetField)
-	{
+	public override void Initialize (PlayerFieldController fieldController, Avatar playerAvatar, PlayerFieldController targetField) {
 		base.Initialize (fieldController, playerAvatar, targetField);
 		string strPlay = "Player ";
 		int playerNumber = fieldController.PlayerNumber;
@@ -22,18 +20,17 @@ public class ControlledAgent : PlayerAgent
 		chargeButton = "Charge " + strPlay + playerNumber;
 	}
 
-	public override void Update (float dt)
-	{
+	public override void Update (float dt) {
 		Vector2 movementVector = Vector2.zero;
 		movementVector.x = Util.Sign(Input.GetAxis (horizontalMoveAxis));
 		movementVector.y = Util.Sign(Input.GetAxis (verticalMoveAxis));
 		//Debug.Log (horizontalMoveAxis + " : " + Input.GetAxis (horizontalMoveAxis));
 		//Debug.Log ("movement vector: " + movementVector.ToString ());
-		bool focus = Input.GetAxis (focusButton)  != 0f;
-		bool fire = Input.GetAxis (fireButton) != 0f;
-		bool charge = Input.GetAxis (chargeButton) != 0f;
+		bool focus = Input.GetButton (focusButton);
+		bool fire = Input.GetButton (fireButton);
+		bool charge = Input.GetButton (chargeButton);
 
-		PlayerAvatar.Move (Input.GetAxis (horizontalMoveAxis), Input.GetAxis (verticalMoveAxis), focus, dt);
+		PlayerAvatar.Move (movementVector.x, movementVector.y, focus, dt);
 		if(fire) {
 			PlayerAvatar.StartFiring();
 		} else {
